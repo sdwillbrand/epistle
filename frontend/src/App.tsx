10efb9bb9616fb04c600a3e54dee9532855807c2
Sono Dennis Willbrand <so.willbrand@gmail.com>
2024-08-19 18:33:30 +0200

    add basic editor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,116 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { FormEvent, useEffect, useState, KeyboardEvent, useRef } from "react";
+import classNames from "classnames";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  const [content, setContent] = useState("");
+  const [lines, setLines] = useState([""]);
+  const [currentLine, setCurrentLine] = useState(0);
+  const inputRef = useRef<HTMLTextAreaElement | null>(null);
 
-    function greet() {
-        Greet(name).then(updateResultText);
+  const handleKeys = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (!inputRef.current) return;
+    const key = event.key;
+    console.log({ pos: inputRef.current.selectionStart });
+    console.log({ key });
+    console.log({ lines });
+    const cursorPos = inputRef.current!.selectionStart;
+
+    if (key === "Enter") {
+      setLines((prev) => {
+        const result = [...prev];
+        result[currentLine] = content;
+        result.push("");
+        return result;
+      });
+      setContent("");
+      setCurrentLine((prev) => prev + 1);
+    } else if (key === "Backspace" && !content) {
+      setCurrentLine((prev) => {
+        const nextLine = Math.max(prev - 1, 0);
+        setContent(lines[nextLine]);
+        setLines((prev) => {
+          const result = [...prev];
+          result.pop();
+          return result;
+        });
+        return nextLine;
+      });
+    } else if (key === "Backspace") {
+      setContent((prev) => {
+        let next = prev.slice(0, cursorPos! - 1) + prev.slice(cursorPos);
+        if (cursorPos === prev.length) {
+          next = prev.slice(0, -1);
+        }
+        setLines((prev) => {
+          const result = [...prev];
+          result[currentLine] = next;
+          return result;
+        });
+        return next;
+      });
+      setTimeout(() => {
+        inputRef.current!.focus();
+        inputRef.current!.setSelectionRange(cursorPos - 1, cursorPos - 1);
+      }, 1);
+    } else if (key.length === 1) {
+      setContent((prev) => {
+        let next = "";
+        if (cursorPos !== 0) {
+          next = prev.slice(0, cursorPos) + key + prev.slice(cursorPos);
+        } else {
+          next = key + prev;
+        }
+
+        setLines((prev) => {
+          const result = [...prev];
+          result[currentLine] = next;
+          return result;
+        });
+        return next;
+      });
+      setTimeout(() => {
+        inputRef.current!.focus();
+        inputRef.current!.setSelectionRange(cursorPos + 1, cursorPos + 1);
+      }, 1);
+    } else if (key === "ArrowDown" && currentLine + 1 < lines.length) {
+      setCurrentLine((prev) => {
+        const nextLine = prev + 1;
+        setContent(lines[nextLine]);
+        return nextLine;
+      });
+    } else if (key === "ArrowUp" && currentLine > 0) {
+      setCurrentLine((prev) => {
+        const nextLine = Math.max(prev - 1, 0);
+        setContent(lines[nextLine]);
+        return nextLine;
+      });
     }
+  };
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+  return (
+    <div id="App" className="px-10 py-5">
+      <div className={classNames("h-full outline-transparent relative")}>
+        {lines.map((line, i) => (
+          <div
+            className={classNames({
+              hidden: i === currentLine,
+            })}
+            key={i}
+            style={{ top: `${i * 20}px`, minHeight: "24px" }}
+          >
+            <span className="m-0 p-0">{line}</span>
+          </div>
+        ))}
+        <textarea
+          ref={inputRef}
+          onKeyDown={handleKeys}
+          className="bg-black text-white outline-none resize-none w-full overflow-hidden z-10 absolute"
+          style={{ top: `${currentLine * 24}px`, height: "24px" }}
+          value={content}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
