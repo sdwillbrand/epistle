6cafc21c8059a3caf05c1d6595d14bd1eaecc6e3
Sono Dennis Willbrand <so.willbrand@gmail.com>
2024-08-19 19:37:00 +0200

    feat: file saving

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 )
 
 // App struct
@@ -24,4 +26,21 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) SaveFile(filename string, file string) bool {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+
+	n, err := f.WriteString(file + "\n")
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+	fmt.Printf("wrote %d bytes\n", n)
+	f.Sync()
+	return true
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import { FormEvent, useEffect, useState, KeyboardEvent, useRef } from "react";
+import { useState, KeyboardEvent, useRef } from "react";
 import classNames from "classnames";
+import { SaveFile } from "../wailsjs/go/main/App";
 
 function App() {
   const [content, setContent] = useState("");
   const [lines, setLines] = useState([""]);
   const [currentLine, setCurrentLine] = useState(0);
+  const [tooltipHidden, setToolTipHidden] = useState(true);
   const inputRef = useRef<HTMLTextAreaElement | null>(null);
 
   const handleKeys = (event: KeyboardEvent<HTMLTextAreaElement>) => {
@@ -12,7 +14,6 @@ function App() {
     const key = event.key;
     console.log({ pos: inputRef.current.selectionStart });
     console.log({ key });
-    console.log({ lines });
     const cursorPos = inputRef.current!.selectionStart;
 
     if (key === "Enter") {
@@ -84,12 +85,45 @@ function App() {
         setContent(lines[nextLine]);
         return nextLine;
       });
+    } else if (key === "Tab") {
+      setContent((prev) => {
+        let next = "";
+        const key = "\t";
+        if (cursorPos !== 0) {
+          next = prev.slice(0, cursorPos) + key + prev.slice(cursorPos);
+        } else {
+          next = key + prev;
+        }
+
+        setLines((prev) => {
+          const result = [...prev];
+          result[currentLine] = next;
+          return result;
+        });
+        return next;
+      });
+      setTimeout(() => {
+        inputRef.current!.focus();
+        inputRef.current!.setSelectionRange(cursorPos + 1, cursorPos + 1);
+      }, 1);
     }
+  };
+
+  const handleSave = () => {
+    SaveFile("test.txt", lines.join("\n")).then(() =>
+      console.log("Successful")
+    );
   };
 
   return (
     <div id="App" className="px-10 py-5">
-      <div className={classNames("h-full outline-transparent relative")}>
+      <button
+        className="p-1 border rounded-md hover:bg-white hover:text-black"
+        onClick={handleSave}
+      >
+        Save
+      </button>
+      <div className="h-full outline-transparent relative mt-5">
         {lines.map((line, i) => (
           <div
             className={classNames({
@@ -98,7 +132,11 @@ function App() {
             key={i}
             style={{ top: `${i * 20}px`, minHeight: "24px" }}
           >
-            <span className="m-0 p-0">{line}</span>
+            {/* !TODO: Seperate line in quotations and tabs */}
+            &nbsp;
+            <span className="m-0 p-0">
+              {[...line].map((c) => (c === "\t" ? "&emsp;" : c))}
+            </span>
           </div>
         ))}
         <textarea
